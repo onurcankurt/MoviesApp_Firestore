46f72e50bbdeea33f1df906473a108a9810c95d2
Onurcan Kurt <onurcankurt96@gmail.com>
2024-05-30 01:26:00 +0300

    mvvm files, firestore, rxswift added

---
+++ MoviesApp_Firestore/data/repo/MoviesDaoRepository.swift
@@ -0,0 +1,12 @@
+//
+//  MoviesDaoRepository.swift
+//  MoviesApp_Firestore
+//
+//  Created by onur on 30.05.2024.
+//
+
+import Foundation
+
+class MoviesDaoRepository {
+    
+}
---
+++ MoviesApp_Firestore/ui/viewmodels/HomePageVM.swift
@@ -0,0 +1,12 @@
+//
+//  HomePageVM.swift
+//  MoviesApp_Firestore
+//
+//  Created by onur on 30.05.2024.
+//
+
+import Foundation
+
+class HomePageVM {
+    
+}
